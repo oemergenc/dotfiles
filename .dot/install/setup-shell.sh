964a7dd26cb6e7c9893f42fbbdd12cfb0c590b50
Oemer Genc <oemergenc@gmail.com>
2019-03-31 19:18:44 +0200

    Update setup-shell.sh

--- .dot/install/setup-shell.sh
+++ .dot/install/setup-shell.sh
@@ -4,7 +4,7 @@
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 
 # SDKMan
-curl -s "https://get.sdkman.io" | zsh
+curl -s "https://get.sdkman.io" | bash
 
 # SpaceVim
 curl -sLf https://spacevim.org/install.sh | bash
