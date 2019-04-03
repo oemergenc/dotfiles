6bc636340537142d819f296309453be48fd5d00e
Oemer Genc <oemergenc@gmail.com>
2019-04-03 22:17:27 +0200

    Update setup-shell.sh

--- .dot/install/setup-shell.sh
+++ .dot/install/setup-shell.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# ZPlug
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+
 # SDKMan
 curl -s "https://get.sdkman.io" | bash
 
